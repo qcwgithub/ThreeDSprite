8c7a9f13175aae02886b37182a9371f2eebf1441
qiucw <qiucw@g-bits.com>
2021-04-29 22:04:06 +0800

    ++++

---
+++ Assets/Editor/MeshSaver.cs
@@ -0,0 +1,104 @@
+﻿using UnityEditor;
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class MeshSaverEditor
+{
+    [MenuItem("Assets/CreateCube")]
+    public static void CreateCube()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = new Vector3[]
+        {
+            // front
+            new Vector3(-0.5f, -0.5f, -0.5f),
+            new Vector3(0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, 0.5f, -0.5f),
+            new Vector3(0.5f, 0.5f, -0.5f),
+
+            // back
+            new Vector3(-0.5f, -0.5f, 0.5f),
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(-0.5f, 0.5f, 0.5f),
+            new Vector3(0.5f, 0.5f, 0.5f),
+        };
+        mesh.triangles = new int[]
+        {
+            // front
+            0,2,1,
+            1,2,3,
+            // back
+            //5,7,4,
+            //7,6,4,
+            // top
+            3,2,6,
+            3,6,7,
+            // bottom
+            //0,1,4,
+            //1,5,4,
+            // left
+            //2,0,4,
+            //2,4,6,
+            // right
+            //1,3,7,
+            //1,7,5,
+        };
+        mesh.uv = new Vector2[]
+        {
+            new Vector2(0f,0f),
+            new Vector2(1f,0f),
+            new Vector2(0f,1-0.4458f),
+            new Vector2(1f,1-0.4458f),
+
+
+            new Vector2(1f,1-0.4458f),
+            new Vector2(0f,1-0.4458f),
+
+            new Vector2(0f,1f),
+            new Vector2(1f,1f),
+        };
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", "MyCube", "asset");
+        if (string.IsNullOrEmpty(path)) return;
+
+        path = FileUtil.GetProjectRelativePath(path);
+        MeshUtility.Optimize(mesh);
+
+        AssetDatabase.CreateAsset(mesh, path);
+        AssetDatabase.SaveAssets();
+    }
+
+    [MenuItem("CONTEXT/MeshFilter/Save Mesh...")]
+    public static void SaveMeshInPlace(MenuCommand menuCommand)
+    {
+        MeshFilter mf = menuCommand.context as MeshFilter;
+        Mesh m = mf.sharedMesh;
+        SaveMesh(m, m.name, false, true);
+    }
+
+    [MenuItem("CONTEXT/MeshFilter/Save Mesh As New Instance...")]
+    public static void SaveMeshNewInstanceItem(MenuCommand menuCommand)
+    {
+        MeshFilter mf = menuCommand.context as MeshFilter;
+        Mesh m = mf.sharedMesh;
+        SaveMesh(m, m.name, true, true);
+    }
+
+    public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
+    {
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+        if (string.IsNullOrEmpty(path)) return;
+
+        path = FileUtil.GetProjectRelativePath(path);
+
+        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+
+        if (optimizeMesh)
+            MeshUtility.Optimize(meshToSave);
+
+        AssetDatabase.CreateAsset(meshToSave, path);
+        AssetDatabase.SaveAssets();
+    }
+
+}
---
+++ Assets/Editor/MyImporter.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+using System.IO;
+
+namespace Assets.Editor
+{
+    public enum MyCubeImporterType
+    {
+        FullCube,
+        Top_Front,
+    }
+
+
+    [ScriptedImporter(1, "cube")]
+    public class MyImporter : ScriptedImporter
+    {
+        public MyCubeImporterType type = MyCubeImporterType.FullCube;
+        [Range(0f, 1f)]
+        public float belowPercent = 0.5f;
+        public int pixelsPerUnit = 100;
+        public Vector3 pivot = new Vector3(0.5f, 0f, 0.5f);
+        public bool needOptimize = true;
+        const float SQRT2 = 1.4142135623730951f;
+
+        Mesh CreateFullCube(float xLength, float yLength, float zLength)
+        {
+            Mesh mesh = new Mesh();
+            var vertices = new Vector3[]
+            {
+                // front
+                new Vector3(-0.5f, -0.5f, -0.5f),
+                new Vector3(0.5f, -0.5f, -0.5f),
+                new Vector3(-0.5f, 0.5f, -0.5f),
+                new Vector3(0.5f, 0.5f, -0.5f),
+
+                // back
+                new Vector3(-0.5f, -0.5f, 0.5f),
+                new Vector3(0.5f, -0.5f, 0.5f),
+                new Vector3(-0.5f, 0.5f, 0.5f),
+                new Vector3(0.5f, 0.5f, 0.5f),
+            };
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i].x *= xLength;
+                vertices[i].y *= yLength;
+                vertices[i].z *= zLength;
+            }
+
+            mesh.vertices = vertices;
+            
+            mesh.triangles = new int[]
+            {
+                // front
+                0,2,1,
+                1,2,3,
+                // back
+                5,7,4,
+                7,6,4,
+                // top
+                3,2,6,
+                3,6,7,
+                // bottom
+                0,1,4,
+                1,5,4,
+                // left
+                2,0,4,
+                2,4,6,
+                // right
+                1,3,7,
+                1,7,5,
+            };
+            mesh.uv = new Vector2[]
+            {
+                new Vector2(0f, 0f),
+                new Vector2(1f, 0f),
+                new Vector2(0f, this.belowPercent),
+                new Vector2(1f, this.belowPercent),
+
+
+                new Vector2(1f, this.belowPercent),
+                new Vector2(0f, this.belowPercent),
+
+                new Vector2(0f, 1f),
+                new Vector2(1f, 1f),
+            };
+
+            if (this.needOptimize)
+            {
+                MeshUtility.Optimize(mesh);
+            }
+            return mesh;
+        }
+
+        Mesh CreateTopFront(float xLength, float yLength, float zLength)
+        {
+            Mesh mesh = new Mesh();
+            mesh.vertices = new Vector3[]
+            {
+                new Vector3(-0.5f, -0.5f, -0.5f),
+                new Vector3(0.5f, -0.5f, -0.5f),
+                new Vector3(-0.5f, 0.5f, -0.5f),
+                new Vector3(0.5f, 0.5f, -0.5f),
+
+                // back
+                new Vector3(-0.5f, 0.5f, 0.5f),
+                new Vector3(0.5f, 0.5f, 0.5f),
+            };
+            mesh.triangles = new int[]
+            {
+                // front
+                0,2,1,
+                1,2,3,
+
+                // top
+                3,2,4,
+                3,4,5,
+            };
+            mesh.uv = new Vector2[]
+            {
+                new Vector2(0f, 0f),
+                new Vector2(1f, 0f),
+                new Vector2(0f, this.belowPercent),
+                new Vector2(1f, this.belowPercent),
+
+                new Vector2(0f, 1f),
+                new Vector2(1f, 1f),
+            };
+
+            if (this.needOptimize)
+            {
+                MeshUtility.Optimize(mesh);
+            }
+            return mesh;
+        }
+
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            // load texture
+            string texturePath = ctx.assetPath.Replace(".cube", ".png");
+            Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
+
+            // new material
+            var material = new Material(Shader.Find("Unlit/Transparent"));
+            material.mainTexture = texture;
+            ctx.AddObjectToAsset("my material", material);
+
+            // new mesh
+
+            float texWidth = texture.width;
+            float texHeight = texture.height;
+
+            float xLength = texWidth / this.pixelsPerUnit;
+            float yLength = texHeight * this.belowPercent * SQRT2 / this.pixelsPerUnit;
+            float zLength = texHeight * (1f - this.belowPercent) * SQRT2 / this.pixelsPerUnit;
+
+            Mesh mesh = null;
+            if (this.type == MyCubeImporterType.FullCube)
+            {
+                mesh = this.CreateFullCube(xLength, yLength, zLength);
+            }
+            else
+            {
+                mesh = this.CreateTopFront(xLength, yLength, zLength);
+            }
+            ctx.AddObjectToAsset("my mesh", mesh);
+
+            // new gameobject
+            string name = Path.GetFileNameWithoutExtension(ctx.assetPath);
+
+            GameObject go = new GameObject(name);
+            ctx.AddObjectToAsset("my go", go);
+            ctx.SetMainObject(go);
+
+            MeshFilter filter = go.AddComponent<MeshFilter>();
+            filter.mesh = mesh;
+
+            MeshRenderer renderer = go.AddComponent<MeshRenderer>();
+            renderer.material = material;
+        }
+    }
+}
