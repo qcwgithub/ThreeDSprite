d71643b47a4fc3b8cee51459f37430232901c2e2
answerwinner <67001868+answerwinner@users.noreply.github.com>
2021-06-21 22:08:07 +0800

    TiledProject/

---
+++ TiledProject/flowers.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="flowers" tilewidth="126" tileheight="105" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="110" height="96" source="flowers/flower_a.png"/>
+ </tile>
+ <tile id="1">
+  <image width="126" height="105" source="flowers/flower_b.png"/>
+ </tile>
+ <tile id="2">
+  <image width="114" height="101" source="flowers/flower_c.png"/>
+ </tile>
+ <tile id="3">
+  <image width="37" height="44" source="flowers/mushroom_tea.png"/>
+ </tile>
+</tileset>
